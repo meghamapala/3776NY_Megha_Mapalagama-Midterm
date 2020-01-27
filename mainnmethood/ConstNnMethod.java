9fedf54fd4144059e2ae311f4867b454bdf837a2
meghamapala <meghamapala@gmail.com>
2020-01-27 15:43:16 -0500

    Construction

---
+++ mainnmethood/ConstNnMethod.java
@@ -0,0 +1,12 @@
+package mainnmethood;
+
+public class ConstNnMethod {
+    public String address = "31-10 37 street";
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void displayAddress() {
+        System.out.println(this.address);
+    }
+}
---
+++ mainnmethood/TestConstNnMethod.java
@@ -0,0 +1,8 @@
+package mainnmethood;
+
+public class TestConstNnMethod {
+    public static void main(String[] args) {
+        ConstNnMethod obj1 = new ConstNnMethod () ;
+        obj1.displayAddress();
+    }
+}
